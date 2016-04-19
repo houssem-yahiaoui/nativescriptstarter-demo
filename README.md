0293773606fd8ebb36042c4e9965500803a85108
Houssem Yahiaoui <houssem.yahiaoui.ai@gmail.com>
2016-04-19 13:04:10 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Scotch.io NativeScript : Ninja Stater How-to
+
+This is a simple DEMO for an article i wrote for scotch.io about the awesome **NativeScript**
+this demo show how we can use the Android **SnackBar** UI element to show a simple message.
+
+Please feel free to download it and use it, but just make sure to install the dependecies like so 
+
+```terminal
+# npm istall
+```
+
+then simple run the app, and you're good to go.
+
+
+Peace :)
---
+++ app/main-page.js
@@ -0,0 +1,19 @@
+
+var frame = require('ui/frame');
+
+function pageLoaded(args) {
+    var page = args.object;
+    page.bindingContext = {};
+}
+exports.pageLoaded = pageLoaded;
+
+function showSnackbar() {
+    
+        var text = "Hi Scotch";
+        var delay = 3500;
+        var snackBar = android.support.design.widget.Snackbar;
+        var snack = snackBar.make(frame.topmost().currentPage.android, text, delay);
+        snack.show();
+    
+};
+exports.showSnackbar = showSnackbar;
